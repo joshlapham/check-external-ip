36e0b01cf66125e111262d1eeb51b34cffb914d6
Josh Lapham <josh@joshlapham.com>
2020-06-03 15:19:30 +1000

    Continue implementing check_ip

--- check_ip.cpp
+++ check_ip.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,53 +11,91 @@ using namespace std;
 using namespace Poco::JSON;
 using namespace Poco::Dynamic;
 
+class StorageFile
+{
+private:
+    std::string _filepath;
 
-// def check_ip_in_storage_file(filepath, response_ip_address):
-//     with open(filepath, 'r') as storage_file:
-//         contents = json.load(storage_file)
-//         last_known_ip = contents['lastKnownIpAddress']
+public:
+    StorageFile(std::string);
 
-//         return last_known_ip
+    std::string checkIpInStorageFile(std::string response_ip_address)
+    {
+        string line;
+        ifstream storage_file(_filepath);
+        Parser parser;
+        std::string jsonContents;
+        Var result;
 
-//     return None
+        // Read contents
+        if (storage_file.is_open())
+        {
+            while (getline(storage_file, line))
+            {
+                cout << line << '\n';
+                result = parser.parse(line);
+            }
 
-
-std::string checkIpInStorageFile(std::string filepath, std::string response_ip_address) {
-    string line;
-    ifstream storage_file(filepath);
-    Parser parser;
-    std::string jsonContents;
-    Var result;
-
-    // Read contents
-    if (storage_file.is_open()) {
-        while (getline(storage_file, line)) {
-            cout << line << '\n';
-            result = parser.parse(line);
+            storage_file.close();
+        }
+        else
+        {
+            cout << "Unable to open file";
+            return "Unable to open file";
         }
 
-        storage_file.close();
+        Object::Ptr object = result.extract<Object::Ptr>();
 
-    } else {
-        cout << "Unable to open file";
-        return "Unable to open file";
+        Var last_known_ip = object->get("lastKnownIpAddress");
+
+        std::string last_known_ip_str = last_known_ip.toString();
+
+        return last_known_ip_str;
     }
+};
 
-    // cout << result;
-
-    Object::Ptr object = result.extract<Object::Ptr>();
-
-    Var last_known_ip = object->get("lastKnownIpAddress");
-
-    std::string last_known_ip_str = last_known_ip.toString();
-
-    return last_known_ip_str;
+StorageFile::StorageFile(std::string filepath)
+{
+    _filepath = filepath;
 }
 
-int main() {
-    std::string last_known_ip = checkIpInStorageFile("check_ip.json", "123.456.0");
+class NoStorageFileException : public exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Storage filepath environment variable was not set; a storage filepath is required";
+    }
+} NoStorageFileException;
 
-    cout << "Last known IP: " << last_known_ip << endl;
+int main()
+{
+    // Parse environment variables
+    // TODO: Throw exception & stop execution if any of these environment variables are null
+    try
+    {
+        const char *url = std::getenv("JL_CHECK_IP_URL");
+        const char *storage_filepath = std::getenv("JL_CHECK_IP_STORAGE_FILEPATH");
+        const char *vpn_profile_filepath = std::getenv("JL_CHECK_IP_VPN_PROFILE_FILEPATH");
 
-    return 0;
+        if (storage_filepath == NULL)
+        {
+            throw NoStorageFileException;
+        }
+
+        cout << url << endl;
+        cout << storage_filepath << endl;
+        cout << vpn_profile_filepath << endl;
+
+        StorageFile storage_file_obj(storage_filepath);
+
+        std::string last_known_ip = storage_file_obj.checkIpInStorageFile("123.456.0");
+
+        cout << "Last known IP: " << last_known_ip << endl;
+
+        return 0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
