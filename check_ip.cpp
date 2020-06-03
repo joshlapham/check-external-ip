98dbc307a5d61bb1dd9cb20d1f80fd592547c667
Josh Lapham <josh@joshlapham.com>
2020-06-03 17:55:28 +1000

    Make network request for IP address

--- check_ip.cpp
+++ check_ip.cpp
@@ -6,10 +6,18 @@
 #include <Poco/JSON/JSON.h>
 #include <Poco/JSON/Parser.h>
 #include <Poco/Dynamic/Var.h>
+#include <Poco/Net/HTTPSClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Path.h>
+#include <Poco/URI.h>
 
 using namespace std;
 using namespace Poco::JSON;
 using namespace Poco::Dynamic;
+using namespace Poco::Net;
+using namespace Poco;
 
 class StorageFile
 {
@@ -77,7 +85,7 @@ int main()
     {
         // TODO: Throw exception & stop execution if any of these environment variables are null
         const char *storage_filepath = std::getenv("JL_CHECK_IP_STORAGE_FILEPATH");
-        // const char *url = std::getenv("JL_CHECK_IP_URL");
+        const char *url = std::getenv("JL_CHECK_IP_URL");
         // const char *vpn_profile_filepath = std::getenv("JL_CHECK_IP_VPN_PROFILE_FILEPATH");
 
         if (storage_filepath == NULL)
@@ -90,11 +98,41 @@ int main()
         // cout << url << endl;
         // cout << vpn_profile_filepath << endl;
 
+        // TODO: Refactor network request logic to own function
+        // Make network request; parse current IP address from response
+        URI uri(url);
+        const Context::Ptr context = new Context(Context::CLIENT_USE, "", "", "", Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+        HTTPSClientSession session(uri.getHost(), uri.getPort(), context);
+        
+        cout << uri.getHost() << endl;
+        cout << uri.getPort() << endl;
+        // cout << session << endl;
+
+        string path(uri.getPathAndQuery());
+        if (path.empty()) path = "/";
+
+        // cout << path << endl;
+
+        HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+
+        session.sendRequest(req);
+
+        // Parse response
+        HTTPResponse res;
+        cout << res.getStatus() << " " << res.getReason() << endl;
+
+        // Print reponse
+        istream &is = session.receiveResponse(res);
+        StreamCopier::copyStream(is, cout);
+
+        // Load storage file; check last known IP address value in storage file
         StorageFile storage_file_obj(storage_filepath);
 
         std::string last_known_ip = storage_file_obj.checkIpInStorageFile("123.456.0");
 
         cout << "Last known IP: " << last_known_ip << endl;
+
+        // TODO: Finish implementing -- compare IP addresses, act accordingly
 
         return 0;
     }
